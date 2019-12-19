7da2c3805bdd7aeb0957d642688acde5dc99cac2
Ormair <milkyahonpo.ship7@gmail.com>
2019-12-19 16:16:43 +0900

    Updates

---
+++ precache-manifest.2c4c2da1e574482813e37548d281812c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36842ead95d6d3f9bbd17c98cc55d2dd",
+    "url": "/milkyahonpo_hp/index.html"
+  },
+  {
+    "revision": "c1dce9c9bea3d6c37f7e",
+    "url": "/milkyahonpo_hp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e35358faa12b7b342d08",
+    "url": "/milkyahonpo_hp/static/js/2.70b3ac82.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/milkyahonpo_hp/static/js/2.70b3ac82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1dce9c9bea3d6c37f7e",
+    "url": "/milkyahonpo_hp/static/js/main.be848b98.chunk.js"
+  },
+  {
+    "revision": "c08a39b926394796ff42",
+    "url": "/milkyahonpo_hp/static/js/runtime-main.cdc58252.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/milkyahonpo_hp/static/media/logo.5d5d9eef.svg"
+  }
+]);
